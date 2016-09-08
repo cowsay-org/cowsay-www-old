2f98117daec0e939572318e33a5c73c4ac1ba9ef
Andrew Janke <andrew@apjanke.net>
2016-09-08 00:02:50 -0400

    add FAQ on .diamonds TLD

---
+++ supplemental/analysis/tld_humor.R
@@ -0,0 +1,20 @@
+tlds <- c('.biz', '.org', '.wtf', '.diamonds', '.ninja')
+means <- c(78,  37,      86,     214,         135)
+stds <- c( 32,  14,      37,     64,          14)
+n <- 87
+
+id = c()
+x = c()
+df = data.frame(id, x)
+for (i in 1:length(tlds)) {
+  x <- rnorm(n = n, mean=means[i], sd=stds[i])
+  id <- rep(tlds[i], n)
+  df <- rbind(df, data.frame(id, x))
+}
+
+filename <- 'gtld_humor_response.png'
+png(filename = filename, width = 800, height = 600, units = "px")
+boxplot(x~id, data = df, main = "Observed gTLD Amusement Response",
+        xlab = "gTLD", ylab = "millichortles/impression",
+        res = 300)
+dev.off()
